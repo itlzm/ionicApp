bfbb218b49986ec5f5142eceb0ad65b36f11a739
itlzm <15221003637@163.com>
2017-03-28 00:40:10 +0800

    first commit

---
+++ src/pages/tabs/tabs.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+import { HomePage } from '../home/home';
+import { BuyPage } from '../buy/buy';
+import { BagPage } from '../bag/bag';
+import { ContactPage } from '../contact/contact';
+
+@Component({
+  templateUrl: 'tabs.html'
+})
+export class TabsPage {
+  // this tells the tabs component which Pages
+  // should be each tab's root Page
+  tab1Root: any = HomePage;
+  tab2Root: any = BuyPage;
+  tab4Root: any = BagPage;
+  tab3Root: any = ContactPage;
+
+  constructor() {
+
+  }
+}
