d8ee60a2cc8bfc14d2f09037d9e0052f4880fd81
itlzm <15221003637@163.com>
2017-03-28 10:17:46 +0800

    修改main.ts

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,5 +1,12 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app.module';
+/**
+ * 在main.ts上加上 import {enableProdMode} from '@angular/core'
+ * 在bootstrap前面加上enableProdMode();
+ * 运行 ionic build Android --prod, 这样启动的速度就会达到5s左右
+ */
+import {enableProdMode} from '@angular/core';
+enableProdMode();
 
 platformBrowserDynamic().bootstrapModule(AppModule);
